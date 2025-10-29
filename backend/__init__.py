535c095d223bce9338113af62b9813e9d14809df
AJprogramming123 <147675413+AJprogramming123@users.noreply.github.com>
2025-10-28 21:09:59 -0400

    Refactor app initialization to use S3
    Removed local file path configurations and added S3 client configuration.

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,22 +1,15 @@
-import os
+import boto3
 from flask import Flask
 from .moving import moving_bp
 
 def create_app():
-    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../frontend'))
-    template_folder = os.path.join(base_dir, 'templates')
-    static_folder = os.path.join(base_dir, 'static')
+    app = Flask(__name__)
 
-    app = Flask(
-        __name__,
-        template_folder=template_folder,
-        static_folder=static_folder
-    )
-
-    # Set path to /downloads at the project root
-    app.config['DOWNLOADS_FOLDER'] = os.path.abspath(
-        os.path.join(os.path.dirname(__file__), '../downloads')
-    )
+    # Configure S3 client
+    app.config['S3_BUCKET'] = 'mycapstonebucket'
+    app.config['S3_FRONTEND_PATH'] = 'frontend'
+    app.config['S3_DOWNLOADS_PATH'] = 'downloads'
+    app.s3 = boto3.client('s3')
 
     app.register_blueprint(moving_bp)
 
