7b58bf3eaad3e6b7cc78714e32bc5ef2b25ad547
AJprogramming123 <147675413+AJprogramming123@users.noreply.github.com>
2025-10-29 00:20:55 -0400

    Refactor S3 template rendering and file serving
    Refactored routes to use a new render_s3_template function for serving HTML templates from S3. Updated static file serving and added a route for downloadable files.

--- backend/moving.py
+++ backend/moving.py
@@ -6,21 +6,32 @@ moving_bp = Blueprint('pages', __name__)
 s3 = boto3.client('s3')
 BUCKET_NAME = 'capstone-ajaimes'
 
-# HTML routes
-@moving_bp.route('/')
-def index():
-    obj = s3.get_object(Bucket=BUCKET_NAME, Key='frontend/templates/index.html')
-    html = obj['Body'].read().decode('utf-8')
-    return render_template_string(html)  # will process any Jinja
-
-@moving_bp.route('/isos')
-def home():
-    obj = s3.get_object(Bucket=BUCKET_NAME, Key='frontend/templates/card.html')
+# --- Serve HTML templates directly from S3 ---
+def render_s3_template(key):
+    obj = s3.get_object(Bucket=BUCKET_NAME, Key=key)
     html = obj['Body'].read().decode('utf-8')
     return render_template_string(html)
 
-# Static assets
+@moving_bp.route('/')
+def index():
+    return render_s3_template('frontend/templates/index.html')
+
+@moving_bp.route('/isos')
+def home():
+    return render_s3_template('frontend/templates/card.html')
+
+@moving_bp.route('/playbooks')
+def about():
+    return render_s3_template('frontend/templates/ansible.html')
+
+# --- Serve static files (CSS, JS, images) from S3 ---
 @moving_bp.route('/static/<path:filename>')
 def static_files(filename):
     obj = s3.get_object(Bucket=BUCKET_NAME, Key=f'frontend/static/{filename}')
-    return send_file(BytesIO(obj['Body'].read()), attachment_filename=filename)
+    return send_file(BytesIO(obj['Body'].read()), download_name=filename)
+
+# --- Serve downloadable files from S3 ---
+@moving_bp.route('/downloads/<filename>')
+def download_file(filename):
+    obj = s3.get_object(Bucket=BUCKET_NAME, Key=f'downloads/{filename}')
+    return send_file(BytesIO(obj['Body'].read()), download_name=filename, as_attachment=True)
