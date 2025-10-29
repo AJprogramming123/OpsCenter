c6a4f25d07d5e943dfeee4ec42fbe0cf1344fa46
AJprogramming123 <147675413+AJprogramming123@users.noreply.github.com>
2025-10-29 00:18:47 -0400

    Refactor routes to use render_template_string and add static file serving
    Updated routes to serve HTML templates using render_template_string and added a new route for serving static files from S3.

--- backend/moving.py
+++ backend/moving.py
@@ -1,46 +1,26 @@
+from flask import Blueprint, Response, send_file, render_template_string
+from io import BytesIO
 import boto3
-from flask import Blueprint, Response
 
 moving_bp = Blueprint('pages', __name__)
-
-# Create S3 client (no credentials needed — IAM handles it)
 s3 = boto3.client('s3')
 BUCKET_NAME = 'capstone-ajaimes'
 
-# --- Serve HTML templates directly from S3 ---
+# HTML routes
 @moving_bp.route('/')
 def index():
     obj = s3.get_object(Bucket=BUCKET_NAME, Key='frontend/templates/index.html')
     html = obj['Body'].read().decode('utf-8')
-    return Response(html, mimetype='text/html')
+    return render_template_string(html)  # will process any Jinja
 
 @moving_bp.route('/isos')
 def home():
     obj = s3.get_object(Bucket=BUCKET_NAME, Key='frontend/templates/card.html')
     html = obj['Body'].read().decode('utf-8')
-    return Response(html, mimetype='text/html')
+    return render_template_string(html)
 
-@moving_bp.route('/playbooks')
-def about():
-    obj = s3.get_object(Bucket=BUCKET_NAME, Key='frontend/templates/ansible.html')
-    html = obj['Body'].read().decode('utf-8')
-    return Response(html, mimetype='text/html')
-
-@moving_bp.route('/docker-scripts')
-def docker_scripts():
-    obj = s3.get_object(Bucket=BUCKET_NAME, Key='frontend/templates/docker.html')
-    html = obj['Body'].read().decode('utf-8')
-    return Response(html, mimetype='text/html')
-
-
-
-
-# --- Serve download files (Ansible, YAML, etc) ---
-@moving_bp.route('/downloads/<filename>')
-def download_file(filename):
-    file_key = f'downloads/{filename}'
-    file_obj = s3.get_object(Bucket=BUCKET_NAME, Key=file_key)
-    return Response(
-        file_obj['Body'].read(),
-        headers={"Content-Disposition": f"attachment; filename={filename}"}
-    )
+# Static assets
+@moving_bp.route('/static/<path:filename>')
+def static_files(filename):
+    obj = s3.get_object(Bucket=BUCKET_NAME, Key=f'frontend/static/{filename}')
+    return send_file(BytesIO(obj['Body'].read()), attachment_filename=filename)
